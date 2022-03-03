93b734eb4c3e76b97c213e0d0e33a07652615aab
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-03-03 17:10:22 -0600

    Achieved add post functionality

--- js/testPage1.js
+++ js/testPage1.js
@@ -1,9 +1,5 @@
 "use strict";
 $(document).ready(function () {
-    $('#test-button-1').click(function (){
-        alert("Test Button 1 was pressed.")
-    });
-
     $.ajax("https://amazing-nasal-juice.glitch.me/posts").done(function(data, status, jqXhr) {
         // $('#loading-message').toggleClass('hidden');
 
@@ -16,10 +12,59 @@ $(document).ready(function () {
                     <p>${element.content}</p>
                     <br>
                 </div>`
-            );//end movie-card-1-container append
-
-
-
+            );
         });//end data.forEach
     });//end ajax .done
+
+    let userDate;
+    let userTime;
+    let userTitle;
+    let userContent
+
+    $('#new-post-submit').click(function (event) {
+        // $('#loading-message').toggleClass('hidden');
+        event.preventDefault();
+
+        userDate = $('#date-input').val();
+        userTime = $('#time-input').val();
+        userTitle = $('#title-input').val();
+        userContent = $('#content-input').val();
+
+        const userAdd = {date: userDate, time: userTime, title: userTitle, content:userContent};
+        const url = 'https://amazing-nasal-juice.glitch.me/posts';
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userAdd),
+        };
+        fetch(url, options)
+            .then(data => console.log(data))
+            .catch( error => console.error(error));
+
+        setTimeout(function () {
+            // $('#loading-message').toggleClass('hidden');
+            $('#post-container').empty();
+
+            $.ajax("https://amazing-nasal-juice.glitch.me/posts").done(function(data, status, jqXhr) {
+                $('#date-input').val('');
+                $('#time-input').val('');
+                $('#title-input').val('');
+                $('#content-input').val('');
+
+                data.forEach(function (element, index) {
+                    $('#post-container').append(
+                        `<div>
+                    <h2>${element.date}</h2>
+                    <p><strong>${element.time}</strong></>
+                    <p><em>${element.title}</em></p>
+                    <p>${element.content}</p>
+                    <br>
+                </div>`
+                    );
+                });
+            });
+        }, 3000);
+    }); //end movie-add-button .click
 }); //end document.ready
